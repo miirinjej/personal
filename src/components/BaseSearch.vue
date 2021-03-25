3f31ae2a9bbfba3ad1843bc2b3cea4272285c17e
miirinjej <miirinjej@gmail.com>
2021-03-25 22:14:57 +0500

    Feat(components): create base search

---
+++ src/components/BaseSearch.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="c-search">
+    <label>
+      <input type="search">
+    </label>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BaseSearch',
+  };
+</script>
+
+<style lang="scss">
+</style>
