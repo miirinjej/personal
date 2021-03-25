2bc78b2dd42e37f54e3010e3b1180141e2ae6961
miirinjej <miirinjej@gmail.com>
2021-03-25 22:33:42 +0500

    Feat(enums): add routing enums

--- src/enums/index.js
+++ src/enums/index.js
@@ -2,10 +2,12 @@ import { readonly } from 'vue';
 
 import LOCALES from './locales';
 import MUTATION_TYPES from './mutation-types';
+import ROUTING from './routing';
 
 const enums = {
   LOCALES,
   MUTATION_TYPES,
+  ROUTING,
 };
 
 export default readonly(enums);
---
+++ src/enums/routing.js
@@ -0,0 +1,29 @@
+const PATH = {
+  ARTICLES: '/',
+  PROJECTS: '/projects',
+  HIRE: '/hire',
+  ABOUT: '/about',
+  CONTACT: '/contact',
+  SITEMAP: '/sitemap',
+  NOT_FOUND: '/not-found',
+};
+
+const NAME = {
+  ARTICLES: 'articles.index',
+  PROJECTS: 'projects.index',
+  HIRE: 'hire.index',
+  ABOUT: 'about.index',
+  CONTACT: 'contact.index',
+  SITEMAP: 'sitemap.index',
+  NOT_FOUND: 'not-found.index',
+};
+
+const ALIAS = {
+  ARTICLES: '/articles',
+};
+
+export default {
+  PATH,
+  NAME,
+  ALIAS,
+};
