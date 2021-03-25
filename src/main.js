42bc0470adc398f3ecc7f85550c60f8e0649bcc9
miirinjej <miirinjej@gmail.com>
2021-03-25 23:12:42 +0500

    Feat: create universal (env-agnostic) app

--- src/main.js
+++ src/main.js
@@ -1,5 +1,42 @@
-import { createApp } from 'vue';
+import { createSSRApp } from 'vue';
 
-import App from './App.vue';
+import { getDefaultLocale, setupI18n } from '@/i18n';
+import App from '@/App.vue';
+import createUniversalRouter from '@/router';
+import createUniversalStore from '@/store';
+import enums from '@/enums';
+// locales
+import en from '@/locales/en.json';
+import ru from '@/locales/ru.json';
 
-createApp(App).mount('#app');
+const { EN } = enums.LOCALES;
+
+export default function createUniversalApp() {
+  const app = createSSRApp(App);
+
+  const i18n = setupI18n({
+    globalInjection: true,
+    legacy: false,
+    locale: getDefaultLocale(),
+    fallbackLocale: import.meta.env.VITE_I18N_FALLBACK_LOCALE || EN,
+    messages: {
+      en,
+      ru,
+    },
+  });
+
+  const store = createUniversalStore();
+  const router = createUniversalRouter(i18n, store);
+
+  app
+    .use(i18n)
+    .use(store)
+    .use(router);
+
+  return {
+    app,
+    i18n,
+    store,
+    router,
+  };
+}
