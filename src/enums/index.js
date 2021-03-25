8984e12fe0da060c9cee767ff5ab79ae0b749e84
miirinjej <miirinjej@gmail.com>
2021-03-25 21:52:03 +0500

    Feat(enums): create locales enums

---
+++ src/enums/index.js
@@ -0,0 +1,9 @@
+import { readonly } from 'vue';
+
+import LOCALES from './locales';
+
+const enums = {
+  LOCALES,
+};
+
+export default readonly(enums);
---
+++ src/enums/locales.js
@@ -0,0 +1,16 @@
+const EN = 'en';
+const RU = 'ru';
+
+const SUPPORTED_LOCALES = [EN, RU];
+
+const LABEL = {
+  [EN]: 'English',
+  [RU]: 'Русский',
+};
+
+export default {
+  EN,
+  RU,
+  SUPPORTED_LOCALES,
+  LABEL,
+};
